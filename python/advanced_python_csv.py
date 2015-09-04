d35450b6fee47bbdb1e37a0f604f9cfbe59715fb
Chuck Daniels <cjdaniels4@gmail.com>
2015-09-03 21:07:51 -0400

    Completed Advanced Python Q5.

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+import advanced_python_regex as apr
+
+emails = sorted(apr.select_email_addresses(apr.read_faculty()))
+
+with open('emails.csv', 'w') as f:
+    for email in emails:
+        f.write(email + '\n')
